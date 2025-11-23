4da0ce3410941f7cc2fc0177a75cd7157334857f
Elchefas <mandorateur@gmail.com>
2025-11-23 14:51:37 +0100

    testchange

--- Backend/EcommerceInventory/EcommerceInventory/urls.py
+++ Backend/EcommerceInventory/EcommerceInventory/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('api/getMenus/', ModuleView.as_view(), name='sidebarmenu'),
     path('api/products/', include('ProductServices.urls')),
 ]
-
+# Serve static files during development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
